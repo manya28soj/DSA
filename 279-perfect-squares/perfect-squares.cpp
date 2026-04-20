e00a0769ff83b72b706152a7e455bf48819b018a
Manya <144474092+manya28soj@users.noreply.github.com>
2026-04-20 21:43:44 +0530

    Time: 76 ms (38.40%) | Memory: 13.9 MB (31.10%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+int solveMem(int n,vector<int> &dp){
+    //base case
+    if(n ==0)
+        return 0;
+
+    if(dp[n] != -1)
+        return dp[n];
+    int ans = n;
+    for( int i = 1; i*i<=n; i++){
+        int temp = i*i;
+        ans= min(ans,1+solveMem(n-temp,dp));
+    }
+    dp[n] =ans;
+    return dp[n];
+}
+
+    int numSquares(int n) {
+        vector<int> dp(n+1,-1);
+        return solveMem(n,dp);
+    }
+};
