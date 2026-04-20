3a49c8d8154f6737b9d62cb1e243d21467b22783
Manya <144474092+manya28soj@users.noreply.github.com>
2026-04-20 21:25:43 +0530

    Time: 4 ms (27.23%) | Memory: 9.1 MB (72.53%) - LeetSync

---
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+int solveMem(vector<int>&nums,int target,vector<int>&dp){
+    //base case
+    if(target < 0)
+        return 0;
+    if(target == 0)
+        return 1;
+    if(dp[target] != -1)
+        return dp[target];
+
+    int ans = 0;
+    for(int i=0; i<nums.size(); i++){
+        ans+=solveMem(nums,target-nums[i],dp);
+    }
+    dp[target] = ans;
+    return dp[target];
+}
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int>dp(target+1,-1);
+        return solveMem(nums,target,dp);
+    }
+};
