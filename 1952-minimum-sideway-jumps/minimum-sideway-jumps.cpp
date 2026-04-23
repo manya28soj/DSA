3037931b3d34d6095d243ee99e6d879352fbffd6
Manya <144474092+manya28soj@users.noreply.github.com>
2026-04-23 13:20:03 +0530

    Time: 59 ms (66.11%) | Memory: 276.1 MB (48.12%) - LeetSync

---
+++ 1952-minimum-sideway-jumps/minimum-sideway-jumps.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+int solve(vector<int>& obstacles,int currlane, int currpos){
+    //base case
+    int n = obstacles.size();
+    if(currpos == n){
+        return 0;
+    }
+    if(obstacles[currpos+1] != currlane){
+        return solve(obstacles,currlane,currpos+1);
+    }
+    else{
+        //sideway jump
+        int ans = INT_MAX;
+
+        for(int i=1;i<=3; i++){
+            if(currlane != i && obstacles[currpos] !=  i)
+                ans = min(ans,1+solve(obstacles,i,currpos));
+        }
+        return ans;
+    }
+
+}
+int solveMem(vector<int>& obstacles, int currlane, int currpos, vector<vector<int>> &dp){
+    //base case
+    int n = obstacles.size()-1;
+    if(currpos == n){
+        return 0;
+    }
+    if(dp[currlane][currpos] != -1)
+        return dp[currlane][currpos];
+    if(obstacles[currpos+1] != currlane){
+        return solveMem(obstacles,currlane,currpos+1,dp);
+    }
+    else{
+        //sideway jump
+        int ans = INT_MAX;
+        for(int i=1;i<=3; i++){
+            if(currlane != i && obstacles[currpos] !=  i)
+                ans = min(ans,1+solveMem(obstacles,i,currpos,dp));
+        }
+        dp[currlane][currpos]=ans;
+        return dp[currlane][currpos];
+    }
+}
+    int minSideJumps(vector<int>& obstacles) {
+        vector<vector<int>> dp(4,vector<int> (obstacles.size(),-1));
+        return solveMem(obstacles,2,0,dp);
+    }
+};
