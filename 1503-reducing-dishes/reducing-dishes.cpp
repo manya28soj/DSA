1d71a332a0866e5f9dc8aea5a001d5614c9be378
Manya <144474092+manya28soj@users.noreply.github.com>
2026-04-24 12:03:10 +0530

    Time: 31 ms (8.70%) | Memory: 25.1 MB (13.29%) - LeetSync

---
+++ 1503-reducing-dishes/reducing-dishes.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+int solveMem(vector<int>& satisfaction, int index, int time, vector<vector<int>> &dp){
+    //base case
+    if(index == satisfaction.size())
+        return 0;
+    if(dp[index][time] != -1)
+        return dp[index][time];
+
+    int include = satisfaction[index] *(time+1) + solveMem(satisfaction, index+1,time+1,dp);
+    int exclude  = 0+solveMem(satisfaction, index+1, time, dp);
+
+    return dp[index][time] = max(include,exclude);
+}
+
+    int maxSatisfaction(vector<int>& satisfaction) {
+        //sort the vector
+        sort(satisfaction.begin() , satisfaction.end());
+
+        int n = satisfaction.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1 ,-1));
+        return solveMem(satisfaction,0,0,dp);
+    }
+};
