4e85548a836dfb0f6e64d80769898c663635685d
Manya <144474092+manya28soj@users.noreply.github.com>
2026-05-04 12:38:20 +0530

    Time: 3 ms (69.85%) | Memory: 15.7 MB (49.75%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void solve(int open, int close, int n, string curr, vector<string>& ans) {
+        // base case
+        if (curr.length() == 2 * n) {
+            ans.push_back(curr);
+            return;
+        }
+
+        // add '('
+        if (open < n) {
+            solve(open + 1, close, n, curr + "(", ans);
+        }
+
+        // add ')'
+        if (close < open) {
+            solve(open, close + 1, n, curr + ")", ans);
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        solve(0, 0, n, "", ans);
+        return ans;
+    }
+};
